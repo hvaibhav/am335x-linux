0894c765fbeeb12f43a43711a6e75508e206b7a6
Stephen Rothwell <sfr@canb.auug.org.au>
2012-09-04 16:54:46 +1000

    Revert "Merge branch 'tcp_fastopen_server'"
    This reverts commit 1bed966cc3bd4042110129f0fc51aeeb59c5b200, reversing
    changes made to 2a35cfa591ac63f17815c2d9432b799e37527980.

--- net/ipv6/syncookies.c
+++ net/ipv6/syncookies.c
@@ -190,7 +190,6 @@ struct sock *cookie_v6_check(struct sock *sk, struct sk_buff *skb)
 	ireq = inet_rsk(req);
 	ireq6 = inet6_rsk(req);
 	treq = tcp_rsk(req);
-	treq->listener = NULL;
 
 	if (security_inet_conn_request(sk, skb, req))
 		goto out_free;
