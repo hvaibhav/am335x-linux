b1466648e6c77f78c9bb90f220beabf069e8817d
Andrew Morton <akpm@linux-foundation.org>
2012-12-09 12:09:05 +1100

    thp-implement-splitting-pmd-for-huge-zero-page-fix
    fix build error
    
    Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2601,7 +2601,6 @@ void __split_huge_page_pmd(struct vm_area_struct *vma, unsigned long address,
 	struct page *page;
 	struct mm_struct *mm = vma->vm_mm;
 	unsigned long haddr = address & HPAGE_PMD_MASK;
-	struct mm_struct *mm = vma->vm_mm;
 	unsigned long mmun_start;	/* For mmu_notifiers */
 	unsigned long mmun_end;		/* For mmu_notifiers */
 
