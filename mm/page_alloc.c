2b5c92e57c2ac7c3a7e12878eef6541233bca397
Marek Szyprowski <m.szyprowski@samsung.com>
2012-12-09 12:09:10 +1100

    mm: use migrate_prep() instead of migrate_prep_local()
    __alloc_contig_migrate_range() should use all possible ways to get all the
    pages migrated from the given memory range, so pruning per-cpu lru lists
    for all CPUs is required, regadless the cost of such operation.  Otherwise
    some pages which got stuck at per-cpu lru list might get missed by
    migration procedure causing the contiguous allocation to fail.
    
    Reported-by: SeongHwan Yoon <sunghwan.yun@samsung.com>
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Acked-by: Michal Nazarewicz <mina86@mina86.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5768,7 +5768,7 @@ static int __alloc_contig_migrate_range(struct compact_control *cc,
 	unsigned int tries = 0;
 	int ret = 0;
 
-	migrate_prep_local();
+	migrate_prep();
 
 	while (pfn < end || !list_empty(&cc->migratepages)) {
 		if (fatal_signal_pending(current)) {
