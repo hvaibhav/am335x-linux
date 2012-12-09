13641a239f4d53f2fad519bb05f59829d12b6ffc
Andrew Morton <akpm@linux-foundation.org>
2012-12-09 12:11:19 +1100

    scatterlist-dont-bug-when-we-can-trivially-return-a-proper-error-fix
    s/E2BIG/EINVAL/
    
    Cc: Nick Bowler <nbowler@elliptictech.com>
    Cc: Maxim Levitsky <maximlevitsky@gmail.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- lib/scatterlist.c
+++ lib/scatterlist.c
@@ -249,7 +249,7 @@ int __sg_alloc_table(struct sg_table *table, unsigned int nents,
 
 #ifndef ARCH_HAS_SG_CHAIN
 	if (WARN_ON_ONCE(nents > max_ents))
-		return -E2BIG;
+		return -EINVAL;
 #endif
 
 	memset(table, 0, sizeof(*table));
