7111573b40a4d2219f24c5f9d32c1132725780bb
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2012-12-09 12:09:01 +1100

    thp-copy_huge_pmd-copy-huge-zero-page-v6
    It's easy to copy huge zero page. Just set destination pmd to huge zero
    page.
    
    It's safe to copy huge zero page since we have none yet :-p
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: "H. Peter Anvin" <hpa@linux.intel.com>
    Cc: Mel Gorman <mel@csn.ul.ie>
    Cc: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -799,6 +799,11 @@ int copy_huge_pmd(struct mm_struct *dst_mm, struct mm_struct *src_mm,
 		pte_free(dst_mm, pgtable);
 		goto out_unlock;
 	}
+	/*
+	 * mm->pagetable lock is enough to be sure that huge zero pmd is not
+	 * under splitting since we don't split the page itself, only pmd to
+	 * a page table.
+	 */
 	if (is_huge_zero_pmd(pmd)) {
 		set_huge_zero_page(pgtable, dst_mm, vma, addr, dst_pmd);
 		ret = 0;
