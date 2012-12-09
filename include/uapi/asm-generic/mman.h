ef95c9e86251afb67c6735b6657edf406360d280
David Rientjes <rientjes@google.com>
2012-12-09 12:08:28 +1100

    MM: Support more pagesizes for MAP_HUGETLB/SHM_HUGETLB v7 fix fix fix fix
    Move the definitions of MAP_HUGE_SHIFT and MAP_HUGE_MASK to mman-common.h
    and fixup the architectures which do not use that file to fix the
    following build failure:
    
    mm/mmap.c: In function 'SYSC_mmap_pgoff':
    mm/mmap.c:1271:15: error: 'MAP_HUGE_SHIFT' undeclared (first use in this function)
    mm/mmap.c:1271:15: note: each undeclared identifier is reported only once for each function it appears in
    mm/mmap.c:1271:33: error: 'MAP_HUGE_MASK' undeclared (first use in this function)
    
    Tested on alpha, hppa, ia64, mips, powerpc, sparc, and x86.
    
    Signed-off-by: David Rientjes <rientjes@google.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/uapi/asm-generic/mman.h
+++ include/uapi/asm-generic/mman.h
@@ -13,18 +13,7 @@
 #define MAP_STACK	0x20000		/* give out an address that is best suited for process/thread stacks */
 #define MAP_HUGETLB	0x40000		/* create a huge page mapping */
 
-/* Bits [26:31] are reserved */
-
-/*
- * When MAP_HUGETLB is set bits [26:31] encode the log2 of the huge page size.
- * This gives us 6 bits, which is enough until someone invents 128 bit address
- * spaces.
- *
- * Assume these are all power of twos.
- * When 0 use the default page size.
- */
-#define MAP_HUGE_SHIFT  26
-#define MAP_HUGE_MASK   0x3f
+/* Bits [26:31] are reserved, see mman-common.h for MAP_HUGETLB usage */
 
 #define MCL_CURRENT	1		/* lock all current mappings */
 #define MCL_FUTURE	2		/* lock all future mappings */
