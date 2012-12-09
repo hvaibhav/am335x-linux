fbea5226ecf249dd36577987b6b523b59b5eea31
Wen Congyang <wency@cn.fujitsu.com>
2012-12-09 12:08:00 +1100

    memory-hotplug: build zonelist if a zone is populated after onlining pages
    After "memory-hotplug: allocate zone's pcp before onlining pages", we
    build zone list before onlining pages to allocate zone's pcp.  But the
    zone doesn't have pages before onlining pages, and the zone is not in
    zonelist, so we still need to build zonelist after onlining pages.
    
    Signed-off-by: Wen Congyang <wency@cn.fujitsu.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Jiang Liu <liuj97@gmail.com>
    Cc: Len Brown <len.brown@intel.com>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Minchan Kim <minchan.kim@gmail.com>
    Cc: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Cc: Yasuaki Ishimatsu <isimatu.yasuaki@jp.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -522,7 +522,9 @@ int __ref online_pages(unsigned long pfn, unsigned long nr_pages)
 	zone->zone_pgdat->node_present_pages += onlined_pages;
 	if (onlined_pages) {
 		node_set_state(zone_to_nid(zone), N_HIGH_MEMORY);
-		if (!need_zonelists_rebuild)
+		if (need_zonelists_rebuild)
+			build_all_zonelists(NULL, NULL);
+		else
 			zone_pcp_update(zone);
 	}
 
